71aa318f58827b068c669f45077f488e5c5c46fb
msurya <msurya@qti.qualcomm.com>
2018-04-23 14:54:28 +0530

    post changed

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
-var port = process.env.PORT || 1337;
+var port = process.env.PORT || 8090;
 var httpServer = require('http').createServer(app);
 httpServer.listen(port, function() {
     console.log('parse-server-example running on port ' + port + '.');
