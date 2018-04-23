e9fcf12ae6a9800b6991ea35f0b56c4d7d1dbd3e
msurya <msurya@qti.qualcomm.com>
2018-04-23 14:18:47 +0530

    node_modules adde3d

--- index.js
+++ index.js
@@ -2,43 +2,43 @@
 // compatible API routes.
 
 var express = require('express');
-// var ParseServer = require('parse-server').ParseServer;
+var ParseServer = require('parse-server').ParseServer;
 var path = require('path');
-// var ParseDashboard = require('parse-dashboard');
-// var Parse = require('parse/node');
-// Parse.initialize("myAppId");
-// Parse.serverURL = 'http://localhost:1337/parse';
-// var databaseUri = process.env.DATABASE_URI || 'mongodb://ds059634.mlab.com:59634/dev';
+var ParseDashboard = require('parse-dashboard');
+var Parse = require('parse/node');
+Parse.initialize("myAppId");
+Parse.serverURL = 'http://localhost:1337/parse';
+var databaseUri = 'mongodb://ds059634.mlab.com:59634/dev';
 
-// if (!databaseUri) {
-//   console.log('DATABASE_URI not specified, falling back to localhost.');
-// }
+if (!databaseUri) {
+  console.log('DATABASE_URI not specified, falling back to localhost.');
+}
 
-// var api = new ParseServer({
-//   databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
-//   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-//   appId: process.env.APP_ID || 'myAppId',
-//   masterKey: process.env.MASTER_KEY || 'hello_master', //Add your master key here. Keep it secret!
-//   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
-//   liveQuery: {
-//     classNames: [] // List of classes to support for query subscriptions
-//   }
-// });
+var api = new ParseServer({
+  databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
+  cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
+  appId: process.env.APP_ID || 'myAppId',
+  masterKey: process.env.MASTER_KEY || 'hello_master', //Add your master key here. Keep it secret!
+  serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
+  liveQuery: {
+    classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
+  }
+});
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
 // javascriptKey, restAPIKey, dotNetKey, clientKey
 
 
-// var dashboard = new ParseDashboard({
-//   "apps": [
-//     {
-//       "serverURL": "http://localhost:1337/parse",
-//       "appId": "myAppId",
-//       "masterKey": "hello_master",
-//       "appName": "MyApp"
-//     }
-//   ]
-// });
+var dashboard = new ParseDashboard({
+  "apps": [
+    {
+      "serverURL": "http://localhost:1337/parse",
+      "appId": "myAppId",
+      "masterKey": "hello_master",
+      "appName": "MyApp"
+    }
+  ]
+});
 
 var app = express();
 
@@ -46,11 +46,11 @@ var app = express();
 app.use(express.static(path.join(__dirname, '/public')));
 
 // make the Parse Dashboard available at /dashboard
-// app.use('/dashboard', dashboard);
+app.use('/dashboard', dashboard);
 
 // Serve the Parse API on the /parse URL prefix
-// var mountPath = process.env.PARSE_MOUNT || '/parse';
-// app.use(mountPath, api);
+var mountPath = process.env.PARSE_MOUNT || '/parse';
+app.use(mountPath, api);
 
 // Parse Server plays nicely with the rest of your web routes
 app.get('/', function(req, res) {
@@ -67,12 +67,10 @@ app.get('/mytest', function (req, res) {
 
 // There will be a test page available on the /test path of your server url
 // Remove this before launching your app
-app.get('/admin', function(req, res) {
+app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '/public/index.html'));
 });
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/index.html'));
-});
+
 var port = process.env.PORT || 1337;
 var httpServer = require('http').createServer(app);
 httpServer.listen(port, function() {
@@ -80,4 +78,4 @@ httpServer.listen(port, function() {
 });
 
 // This will enable the Live Query real-time server
-// ParseServer.createLiveQueryServer(httpServer);
+ParseServer.createLiveQueryServer(httpServer);
